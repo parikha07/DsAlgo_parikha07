4eb045a47283b2101e714a6ec7dce4c38c5817d9
Parikha Goyanka <34074262+parikha07@users.noreply.github.com>
2020-06-18 21:09:08 +0530

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+int maxProfit(int seats[], int k, int n) 
+{ 
+	priority_queue<int> pq; 
+	for (int i = 0; i < k; i++) 
+		pq.push(seats[i]); 
+	int profit = 0; 
+	int c = 0; 
+	while (c < n) { 
+		int top = pq.top(); 
+		pq.pop(); 
+		if (top == 0) 
+			break;
+		profit = profit + top; 
+		pq.push(top - 1); 
+		c++; 
+	} 
+	return profit; 
+} 
+int main() 
+{ 
+	int seats[] = { 2, 3, 4, 5, 1 }; 
+	int k = sizeof(seats) / sizeof(int); 
+	int n = 6; 
+
+	cout << maxProfit(seats, k, n); 
+
+	return 0; 
+} 
